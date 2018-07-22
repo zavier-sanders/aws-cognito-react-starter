1b39f6636e89b85460a9f5adfc202296e73dec02
Zavier <zav765@gmail.com>
2018-07-22 12:48:01 -0500

    Update Robinhood.js

--- src/db/Robinhood.js
+++ src/db/Robinhood.js
@@ -3,11 +3,6 @@ import AWS from 'aws-sdk'
 
 AWS.config.region = CONFIG.Region;
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: CONFIG.CognitoIdentityPool});
-AWS.config.update({
-  accessKeyId: "AKIAIKW5CUMSVU6ECGPA",
-  secretAccessKey: "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",
-  "region": "us-east-1" 
-});
 
 let docClient = new AWS.DynamoDB.DocumentClient({
   region: CONFIG.Region
